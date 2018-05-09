8cbd222f8ee2ba616c2305660f36347ebbd6ee97
lusuon <542343357@qq.com>
2018-05-09 23:13:05 +0800

    commit

--- src/Catalog.java
+++ src/Catalog.java
@@ -7,26 +7,32 @@ The class Catalog models a product catalog. This class implements the interface 
 //public Iterator<Product> iterator() — Returns an iterator over the instances in the collection products.
 
 import java.util.ArrayList;
-
-public Iterator<Product> iterator()
+import java.util.Collection;
+import java.util.Iterator;
 
 
 public class Catalog {
-    ArrayList products;
-
+    Collection<Product> products
     //public Catalog() — Creates the collection products, which is initially empty.
     public Catalog(){
-        products = new Product[]
+        products = new ArrayList<Product>();
     }
 
     //public void addProduct(Product product) — Adds the specified product to the collection products.
-    public void addProduct(Product){
-
+    public void addProduct(Product AddProduct){
+        products.add(AddProduct);
     }
 
     //public Product getProduct(String code) — Returns a reference to the Product instance with the specified code. Returns null if there are no products in the catalog with the specified code.
     public Product getProduct(String code){
+        Iterator<Product> iter = products.iterator();
+        while (iter.hasNext()){
+            if (iter.next().getCode()==code){
+                return iter.next();
+                break;
+            }else if(iter.hasNext()==false)
 
+        }
     }
 
     //public int getNumberOfProducts() — Returns the number of instances in the collection products.
